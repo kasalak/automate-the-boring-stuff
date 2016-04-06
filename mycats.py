496c7c2210186606504e30884d87e10199ca4f4c
Karthik <karthik.kasala@gmail.com>
2016-04-06 14:12:22 -0400

    better way of entering cat names

---
+++ mycats.py
@@ -0,0 +1,7 @@
+mypets = ['Zophie', 'Pooka', 'Fat-tail']
+print('Enter a pet name: ')
+name = input()
+if name not in mypets:
+    print('I do not have a pet named ' + name)
+else:
+    print(name + ' is my pet.x`')
